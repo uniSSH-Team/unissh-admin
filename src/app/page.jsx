f02b6e75071e353b68d1d2af797ae87575bbc73e
juaneth <euanwalker2008@gmail.com>
2024-01-18 01:00:47 +0000

    small baseline

---
+++ src/app/page.jsx
@@ -0,0 +1,69 @@
+export default function Home() {
+  return (
+    <main className="h-screen w-screen">
+      <div className="navbar min-h-min h-[40px] drag bordered-b" id="navbar">
+        <div
+          className="navbar-start ml-2 flex flex-row gap-4"
+          id="navbar_start"
+        >
+          <div className="dropdown no-drag" id="navbar_start_btn">
+            <div tabIndex={0} role="button" className="">
+              <h1
+                to={"/"}
+                className="transition-all text-xs text-white/50 hover:text-white/70"
+              >
+                home
+              </h1>
+            </div>
+          </div>
+
+          <div className="dropdown no-drag h-full" id="navbar_start_btn_about">
+            <div tabIndex={0} role="button" className="align-middle">
+              <h1 className="transition-all text-xs text-white/50 hover:text-white/70">
+                about
+              </h1>
+            </div>
+            <ul className="dropdown-content bordered-all mt-3 z-[1] shadow bg-neutral rounded-lg w-64 p-4">
+              <h1>uniSSH Admin Dashboard</h1>
+              <a className="text-sm">Dashboard for analytics</a>
+              <h1 className="text-xs">
+                Made by{" "}
+                <a
+                  className="transition-all hover:text-white/50 after:content-['_↗']"
+                  href=""
+                >
+                  {" "}
+                  Juaneth{" "}
+                </a>
+              </h1>
+            </ul>
+          </div>
+        </div>
+
+        <div className="navbar-center no-drag">
+          <div className="tooltip tooltip-bottom" data-tip="v0.0.1">
+            <a className="transition-all select-none text-sm hover:text-white/60">
+              uniSSH Admin Dashboard
+            </a>
+          </div>
+        </div>
+
+        <div className="navbar-end"></div>
+      </div>
+
+      <div className="stats shadow-xl bg-black">
+        <div className="stat">
+          <div className="stat-title">Montly active users</div>
+          <div className="stat-value">31K</div>
+          <div className="stat-desc">Last 31 Days</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">New Users</div>
+          <div className="stat-value">4</div>
+          <div className="stat-desc"></div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
+
+    daisyui: {
+        themes: [{
+            'main': {
+                "primary": "#111111",
+                "secondary": "#070707",
+                "accent": "#1f2937",
+                "neutral": "#1B1B1B",
+                "base-100": "#111111",
+                "info": "#1d4ed8",
+                "success": "#16a34a",
+                "warning": "#ea580c",
+                "error": "#B44141"
+            },
+        }, ],
+    },
+    plugins: [require("daisyui")],
 }
