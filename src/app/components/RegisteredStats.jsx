5f68ce87ead2cba0a35afda53e3a8ebdc9cc7a27
juaneth <euanwalker2008@gmail.com>
2024-01-24 01:57:33 +0000

    auth problems

--- src/app/components/RegisteredStats.jsx
+++ src/app/components/RegisteredStats.jsx
@@ -1,14 +1,15 @@
-
-
 export default async function RegisteredStats() {
-
   let data = await (
     await fetch("https://unissh-admin.vercel.app/api/analytics", {
       next: {
         revalidate: 60,
       },
     })
-  ).json();
+  )
+    .json()
+    .catch((err) => {
+      console.log(err);
+    });
 
   let popularVersion = Object.keys(
     await data.byVersion.reduce((prev, current) => {
@@ -25,31 +26,31 @@ export default async function RegisteredStats() {
 
   return (
     <>
-      <div className="flex flex-col p-4 gap-3">
-        <div className="">
-          <div className="label pt-0 font-semibold text-xl text-shadow-white">
+      <div className='flex flex-col p-4 gap-3'>
+        <div className=''>
+          <div className='label pt-0 font-semibold text-xl text-shadow-white'>
             Register Statistics:
           </div>
-          <div className="stats shadow-xl bg-black">
-            <div className="stat">
-              <div className="stat-title">Total Registered</div>
-              <div className="stat-value">{data.totalRegistered}</div>
-              <div className="stat-desc">From Analytics API</div>
+          <div className='stats shadow-xl bg-black'>
+            <div className='stat'>
+              <div className='stat-title'>Total Registered</div>
+              <div className='stat-value'>{data.totalRegistered}</div>
+              <div className='stat-desc'>From Analytics API</div>
             </div>
 
-            <div className="stat">
-              <div className="stat-title">New Users</div>
-              <div className="stat-value">{data.lastMonth.totalRegistered}</div>
-              <div className="stat-desc">Last 31 Days</div>
+            <div className='stat'>
+              <div className='stat-title'>New Users</div>
+              <div className='stat-value'>{data.lastMonth.totalRegistered}</div>
+              <div className='stat-desc'>Last 31 Days</div>
             </div>
 
-            <div className="stat">
-              <div className="stat-title">Most popular version:</div>
-              <div className="stat-value">{popularVersion}</div>
+            <div className='stat'>
+              <div className='stat-title'>Most popular version:</div>
+              <div className='stat-value'>{popularVersion}</div>
               <a
                 href={latestRelease.html_url}
-                target="_blank"
-                className="stat-desc hover:text-white/30 transition-all"
+                target='_blank'
+                className='stat-desc hover:text-white/30 transition-all'
               >
                 Latest: {latestRelease.tag_name}
               </a>
